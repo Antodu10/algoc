c06285579f9978b2105730941fe7fb9488401ec2
Antonin <piat.antonin02@gmail.com>
2023-09-26 21:47:55 +0200

    Maj TP du 26/09

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -15,13 +15,15 @@
 #include <string.h>
 #include <errno.h>
 #include <assert.h>
-
+//tableau avec chaques cases est une struct à 3 champs
 
 typedef struct{
 	int annee;
 	char * nom;
 	char * nature;
 }Winner;
+
+
 
 
 int numberOfWinners(FILE * fichier){
@@ -35,10 +37,83 @@ int numberOfWinners(FILE * fichier){
 		fclose(fichier);
 		return counter;
 }
+int* tailleGagnant(FILE* f, int i){
+	int * tab[numberOfWinners(f)];
+	char character;
+	int countVirgule =0;
+	int nbGagnant=0;
+	while ((character=fgetc(f))!=EOF)
+	{
+		if(character==';'){
+			countVirgule+=1;
+			if(countVirgule&2==1){
+				
+			}
 
-int main(void)
+		}
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+Winner ** creerFichier(FILE* fichier){
+	int nbWinners = numberOfWinners(fichier);
+	Winner * F[]= (Winner *)malloc(sizeof(Winner)*nbWinners);
+		for(int i=0;i<nbWinners;i++){
+			F[i].annee= (int)malloc(sizeof(int));
+			F[i].nom=(char*)malloc(sizeof(char));
+		} 
+		
+
+}
+int lengthStringFromFileUntil(FILE*f,char delim){
+	int result =0;
+	char character;
+	while((character=fgetc(f))!=delim);
+		delim+=1;
+}
+
+char * readStringFromFileUntil(FILE* f, char delim){
+	int length = lengthStringFromFileUntil(f,delim);
+	char * result=calloc(length+1,sizeof(char));
+	int poubelle = fseek(f, -length,0);
+	char character;
+	while ((character=fgetc(f))!=delim)
+	{
+		
+	}
+	
+
+
+}
+
+void readWinner(Winner * winner , FILE*f)
+{
+	fscanf(f,"%i"";",&winner->annee);
+	winner->nom =readStringFromFileUntil(f,';');
+	winner->nature=readStringFromFileUntil(f,'\n');
+}
+
+void readWinners(FILE * f){
+	
+	}
+	
+}
+
+int main(int argc, char** argv)
 { 
 	char filename[] = "turingWinners.csv";
+	char outputFilename[]="out.csv";
 	FILE* f = fopen(filename,"r");
     // TODO
 	printf("La valeur de l'entier est : %d\n", numberOfWinners(f));
