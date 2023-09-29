d1d20bf8dce6e11f9672c25d6e950c5d2bb7a49c
Antonin <piat.antonin02@gmail.com>
2023-09-29 17:22:09 +0200

    modife code

--- TP-03-PrixTuring/prixTuring.c
+++ TP-03-PrixTuring/prixTuring.c
@@ -23,8 +23,26 @@ typedef struct{
 	char * nature;
 }Winner;
 
+void deplacerCurseur(FILE* f, int num_winner, int partie){
+	rewind(f); //on repart de 0
+	int num_winner_actif=1;
+	int partie_active =1;
+	char character = ' ';
+//on se déplace au bon winner
+	while(num_winner_actif!=num_winner){
+		if((character=fgetc(f))=='\n'){
+			num_winner_actif++;
+		}
+	}
+//on se déplace au bon caractère
+	while(partie_active!=partie){
+		if((character=fgetc(f))==';'){
+			partie_active++;
+		}
+	}
+//le curseur est placé sur la virgule juste avant l'info souhaité	
 
-
+}
 
 int numberOfWinners(FILE * fichier){
 	char character;
@@ -37,86 +55,61 @@ int numberOfWinners(FILE * fichier){
 		fclose(fichier);
 		return counter;
 }
-int* tailleGagnant(FILE* f, int i){
-	int * tab[numberOfWinners(f)];
-	char character;
-	int countVirgule =0;
-	int nbGagnant=0;
-	while ((character=fgetc(f))!=EOF)
-	{
-		if(character==';'){
-			countVirgule+=1;
-			if(countVirgule&2==1){
-				
-			}
 
-		}
+int compterTaille(FILE* f, int num_winner, int partie){
+	deplacerCurseur(f,num_winner,partie);
+	int compteur = 0;
+	char character = ' ';
+	while(character!=EOF&&character!=';'&&character!='\n'){
+		compteur++;
+		character=fgetc(f);
 	}
-	
+	return compteur;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-Winner ** creerFichier(FILE* fichier){
-	int nbWinners = numberOfWinners(fichier);
-	Winner * F[]= (Winner *)malloc(sizeof(Winner)*nbWinners);
-		for(int i=0;i<nbWinners;i++){
-			F[i].annee= (int)malloc(sizeof(int));
-			F[i].nom=(char*)malloc(sizeof(char));
-		} 
-		
+void creer_winner(Winner ** tableau_winner, FILE* f,int i){
+		tableau_winner[i]= malloc(sizeof(Winner));
+		int taille_nom= compterTaille(f,i,2);
+		int taille_nature= compterTaille(f,i,3);
+		//on créer de la mémoire
+		tableau_winner[i]->nom=calloc(taille_nom+1,sizeof(char));
+		tableau_winner[i]->nature=calloc(taille_nature+1,sizeof(char));
+		deplacerCurseur(f,i,1); 
+		//on replace le curseur
+		fscanf(f,"%d",&(tableau_winner[i]->annee));
+		deplacerCurseur(f,i,2);
+		fgets(tableau_winner[i]->nom, taille_nom, f);
+		deplacerCurseur(f,i,3);
+		fgets(tableau_winner[i]->nature, taille_nature, f);
 
 }
-int lengthStringFromFileUntil(FILE*f,char delim){
-	int result =0;
-	char character;
-	while((character=fgetc(f))!=delim);
-		delim+=1;
+
+void remplirTableau(Winner** tab_winner, FILE* f, int number_of_winners){
+
+	for(int i; i<number_of_winners;i++){
+		creer_winner(tab_winner,f,i+1);
+	}
 }
 
-char * readStringFromFileUntil(FILE* f, char delim){
-	int length = lengthStringFromFileUntil(f,delim);
-	char * result=calloc(length+1,sizeof(char));
-	int poubelle = fseek(f, -length,0);
-	char character;
-	while ((character=fgetc(f))!=delim)
-	{
-		
+Winner ** tableau_winner (FILE* f){
+	int number_of_winners = numberOfWinners(f);
+	Winner** tab_winners = calloc(number_of_winners, sizeof(Winner*));
+	remplirTableau(tab_winners,f,number_of_winners);
+	return tab_winners;
+}
+
+Winner ** readWinners(FILE * f){
+	return tableau_winner(f);
 	}
 	
 
 
-}
-
-void readWinner(Winner * winner , FILE*f)
-{
-	fscanf(f,"%i"";",&winner->annee);
-	winner->nom =readStringFromFileUntil(f,';');
-	winner->nature=readStringFromFileUntil(f,'\n');
-}
-
-void readWinners(FILE * f){
-	
-	}
-	
-}
-
-int main(int argc, char** argv)
+int main(void)
 { 
 	char filename[] = "turingWinners.csv";
-	char outputFilename[]="out.csv";
 	FILE* f = fopen(filename,"r");
     // TODO
 	printf("La valeur de l'entier est : %d\n", numberOfWinners(f));
-
+	Winner ** liste = readWinners(f);
 	return EXIT_SUCCESS;
 }
